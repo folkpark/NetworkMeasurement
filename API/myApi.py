a7bb9f134e5b8cf14e4d8dd4051e7cccff116c34
folkpark <parkerfolkman@gmail.com>
2019-01-28 11:05:30 -0700

    Simple API GET

--- API/myApi.py
+++ API/myApi.py
@@ -8,4 +8,5 @@ app.config["DEBUG"] = True
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
