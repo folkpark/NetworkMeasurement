45156032fd47a206514e3f4a0a7cea87cd97c2e2
folkpark <parkerfolkman@gmail.com>
2019-01-28 11:30:36 -0700

    Simple API GET

--- API/myApi.py
+++ API/myApi.py
@@ -1,6 +1,6 @@
 import flask
 
-app = flask.Flask(__name__,static_url_path='/NetworkMeasurement/API/')
+app = flask.Flask(__name__,static_url_path='')
 app.config["DEBUG"] = True
 
 
@@ -8,9 +8,9 @@ app.config["DEBUG"] = True
 # def home():
 #     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-@app.route('/NetworkMeasurement/API/<path:path>',  methods=['GET'])
-def send_js(path):
-    return flask.send_from_directory('homePage.html', path)
+@app.route('/',  methods=['GET'])
+def home():
+    return app.send_static_file('/NetworkMeasurement/API/homePage.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
