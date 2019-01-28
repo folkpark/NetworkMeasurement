ce79d94f67948d6473db48c22fa047f417ecb278
folkpark <parkerfolkman@gmail.com>
2019-01-28 12:00:24 -0700

    Simple API GET

--- API/myApi.py
+++ API/myApi.py
@@ -7,10 +7,9 @@ app.config["DEBUG"] = True
 def return_files_tut():
     try:
         return flask.send_file('/home/ubuntu/NetworkMeasurement/API/parker.jpg',
-                               attachment_filename='test.txt')
+                               attachment_filename='parker.jpg')
     except Exception as e:
         return str(e)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
