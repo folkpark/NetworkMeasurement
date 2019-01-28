4f875e40c40600b0a1f709b5ad31efe1034910e1
folkpark <parkerfolkman@gmail.com>
2019-01-27 17:10:07 -0700

    wireshark added

--- simpleSocket/serverSocket.py
+++ simpleSocket/serverSocket.py
@@ -1,16 +1,12 @@
 # Echo server program
 import socket
-import time
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 5500              # Arbitrary non-privileged port
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
-    start = time.time()
     conn, addr = s.accept()
-    end = time.time()
-    print(end - start)
     with conn:
         print('Connected by', addr)
         while True:
