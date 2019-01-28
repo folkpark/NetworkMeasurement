5860d89bb28a58c3045ed2a5b7ce097cbdc51add
folkpark <parkerfolkman@gmail.com>
2019-01-28 11:48:37 -0700

    Simple API GET

--- API/myApi.py
+++ API/myApi.py
@@ -1,6 +1,6 @@
 import flask
 
-app = flask.Flask(__name__,static_url_path='')
+app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
@@ -8,11 +8,20 @@ app.config["DEBUG"] = True
 # def home():
 #     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-@app.route('/home/ubuntu/NetworkMeasurement/API/<path:filename>')
-def home(filename):
-    return flask.send_from_directory(app.config['/home/ubuntu/NetworkMeasurement/API/test.txt'],
-                                     filename,
-                                     as_attachment=True)
+# @app.route('/home/ubuntu/NetworkMeasurement/API/<path:filename>')
+# def home(filename):
+#     return flask.send_from_directory(app.config['/home/ubuntu/NetworkMeasurement/API/test.txt'],
+#                                      filename,
+#                                      as_attachment=True)
+
+@app.route('/API/')
+def return_files_tut():
+    try:
+        return flask.send_file('/home/ubuntu/NetworkMeasurement/API/',
+                               attachment_filename='test.txt')
+    except Exception as e:
+        return str(e)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
